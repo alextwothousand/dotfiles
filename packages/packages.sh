c9cfe45db597d20e52d29d905dae70174a9c0d47
alex <38814044+alextwothousand@users.noreply.github.com>
2021-03-09 22:16:01 +0000

    Update packages.sh

--- packages/packages.sh
+++ packages/packages.sh
@@ -13,3 +13,9 @@ sudo yum install -y util-linux-user
 # zsh and oh-my-zsh
 sudo yum install -y zsh
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
+# social media
+sudo yum install -y Discord-installer
+
+# dev tools
+sudo yum install -y jetbrains-clion
