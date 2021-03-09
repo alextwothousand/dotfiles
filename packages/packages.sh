ce6c73f404dcacde2a17c95bdac3bb36d677fc52
alex <38814044+alextwothousand@users.noreply.github.com>
2021-03-09 22:53:29 +0000

    Update packages.sh

--- packages/packages.sh
+++ packages/packages.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# vscodium repo
+sudo tee -a /etc/yum.repos.d/vscodium.repo << 'EOF'
+[gitlab.com_paulcarroty_vscodium_repo]
+name=gitlab.com_paulcarroty_vscodium_repo
+baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+metadata_expire=1h
+EOF
+
+
 # rpm fusion
 sudo yum install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
@@ -23,11 +36,12 @@ sudo yum install -y telegram-desktop
 
 # dev tools
 sudo yum install -y jetbrains-clion
+sudo yum install -y codium
 
 # 0x0 wrapper
 curl -LO https://raw.githubusercontent.com/Calinou/0x0/master/bin/0x0
 sudo chmod +x 0x0
 sudo mv 0x0 /usr/local/bin
 
-# otherr
+# other
 sudo yum install -y exa jetbrains-mono-fonts
