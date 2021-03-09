33e8260b4e750a634ee31754e332b802f7670f98
alex <38814044+alextwothousand@users.noreply.github.com>
2021-03-09 22:14:59 +0000

    Create packages.sh

---
+++ packages/packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# add repos from copr
+sudo dnf copr enable peterwu/jetbrains -y
+sudo dnf copr enable dvdmuckle/discord -y
+
+# curl wget
+sudo yum install -y curl wget
+
+# util-linux-user (for chsh)
+sudo yum install -y util-linux-user
+
+# zsh and oh-my-zsh
+sudo yum install -y zsh
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
